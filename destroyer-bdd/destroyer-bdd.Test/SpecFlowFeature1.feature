dd467ebaa38146bdeb8d2717c19530d90601cd6e
guywithlumia <dimanych96@mail.ru>
2018-05-28 09:37:56 +0700

    add test project with specflow

---
+++ destroyer-bdd/destroyer-bdd.Test/SpecFlowFeature1.feature
@@ -0,0 +1,6 @@
+﻿Feature: SpecFlowFeature1
+
+Scenario: Add new file
+	Given I have my destroyer
+	When I add new file path
+	Then File count is 1
---
+++ destroyer-bdd/destroyer-bdd.Test/SpecFlowFeature1Steps.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechTalk.SpecFlow;
+
+namespace destroyer_bdd.Test
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        [Given(@"I have my destroyer")]
+        public void GivenIHaveMyDestroyer()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I add new file path")]
+        public void WhenIAddNewFilePath()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"File count is (.*)")]
+        public void ThenFileCountIs(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}

46308bfb2bdff341cb7919c46db97c65b93c3f4f
guywithlumia <dimanych96@mail.ru>
2018-05-28 09:41:49 +0700

    add Destroyer class

--- destroyer-bdd/destroyer-bdd.Test/SpecFlowFeature1Steps.cs
+++ destroyer-bdd/destroyer-bdd.Test/SpecFlowFeature1Steps.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TechTalk.SpecFlow;
+using destroyer_bdd;
 
 namespace destroyer_bdd.Test
 {
     [Binding]
     public class SpecFlowFeature1Steps
     {
+        Destroyer dest = new Destroyer();
+
         [Given(@"I have my destroyer")]
         public void GivenIHaveMyDestroyer()
         {
             ScenarioContext.Current.Pending();
+
+            Assert.IsNotNull(dest);
         }
         
         [When(@"I add new file path")]
--- destroyer-bdd/destroyer-bdd/Program.cs
+++ destroyer-bdd/destroyer-bdd/Program.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace destroyer_bdd
 {
+    public class Destroyer
+    {
+
+    }
+
     class Program
     {
         static void Main(string[] args)

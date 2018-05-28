5fbaaf0a5317e8fc34808c09792f839fedeef137
guywithlumia <dimanych96@mail.ru>
2018-05-28 09:15:43 +0700

    create project

---
+++ destroyer-bdd/destroyer-bdd/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace destroyer_bdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
